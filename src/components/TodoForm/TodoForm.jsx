71e66170a88ae62e74c2f30549f29afc38d3bec1
Maksym <praniknumberone@gmail.com>
2026-06-18 02:21:10 +0200

    minor bug fixes

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,18 +1,18 @@
-import { useDispatch } from 'react-redux';
-import { addTodo } from '../../store/todoSlice';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import classNames from 'classnames';
-import './TodoForm.scss';
-import { todoValidationSchema } from '../../utils/validation';
+import { useDispatch } from "react-redux";
+import { addTodo } from "../../store/todoSlice";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import classNames from "classnames";
+import "./TodoForm.scss";
+import { todoValidationSchema } from "../../utils/validation";
 
 const validationSchema = Yup.object().shape({
   value: Yup.string()
-    .trim() 
-    .min(3, 'Мінімум 3 символи')
-    .max(150, 'Максимум 150 символів')
-    .required('Обов\'язкове поле! Введіть назву задачі.'),
-  deadline: Yup.date().nullable(), 
+    .trim()
+    .min(3, "Мінімум 3 символи")
+    .max(150, "Максимум 150 символів")
+    .required("Обов'язкове поле! Введіть назву задачі."),
+  deadline: Yup.date().nullable(),
 });
 
 const TodoForm = () => {
@@ -21,44 +21,40 @@ const TodoForm = () => {
   return (
     <div className="todo-form-container">
       <Formik
-        initialValues={{ value: '', deadline: '' }}
+        initialValues={{ value: "", deadline: "" }}
         validationSchema={validationSchema}
         onSubmit={(values, { resetForm }) => {
-          
-          
           const cleanValues = {
             ...values,
             value: values.value.trim(),
           };
-          
+
           dispatch(addTodo(cleanValues));
-          
+
           resetForm();
         }}
       >
         {({ errors, touched }) => (
           <Form className="todo-form">
-            
             <div className="input-group">
               <Field
                 type="text"
                 name="value"
                 placeholder="Що потрібно зробити?"
-              
-                className={classNames('form-input', {
-                  'form-input--error': errors.value && touched.value,
+                className={classNames("form-input", {
+                  "form-input--error": errors.value && touched.value,
                 })}
               />
-            
-              <ErrorMessage name="value" component="div" className="error-text" />
+
+              <ErrorMessage
+                name="value"
+                component="div"
+                className="error-text"
+              />
             </div>
 
             <div className="input-group">
-              <Field
-                type="date"
-                name="deadline"
-                className="form-input"
-              />
+              <Field type="date" name="deadline" className="form-input" />
             </div>
 
             <button type="submit" className="submit-btn">
--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,24 +1,23 @@
-
-import { useDispatch } from 'react-redux';
-import { toggleTodo, deleteTodo } from '../../store/todoSlice';
-import classNames from 'classnames';
-import { isOverdue } from '../../utils/helpers';
+import { useDispatch } from "react-redux";
+import { toggleTodo, deleteTodo } from "../../store/todoSlice";
+import classNames from "classnames";
+import { isOverdue } from "../../utils/helpers.js";
 
 const TodoItem = ({ todo }) => {
   const dispatch = useDispatch();
 
   return (
-    <li 
-      className={classNames('todo-item', {
-        'todo-item--done': todo.isDone,
-        'todo-item--overdue': isOverdue(todo.deadline, todo.isDone)
+    <li
+      className={classNames("todo-item", {
+        "todo-item--done": todo.isDone,
+        "todo-item--overdue": isOverdue(todo.deadline, todo.isDone),
       })}
     >
       <div className="todo-content">
-        <input 
-          type="checkbox" 
-          checked={todo.isDone} 
-          onChange={() => dispatch(toggleTodo(todo.id))} 
+        <input
+          type="checkbox"
+          checked={todo.isDone}
+          onChange={() => dispatch(toggleTodo(todo.id))}
         />
         <span className="todo-text">{todo.value}</span>
         {todo.deadline && (
@@ -27,8 +26,8 @@ const TodoItem = ({ todo }) => {
           </span>
         )}
       </div>
-      <button 
-        className="delete-btn" 
+      <button
+        className="delete-btn"
         onClick={() => dispatch(deleteTodo(todo.id))}
       >
         ❌
